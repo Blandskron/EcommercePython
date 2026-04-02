d1af45ff9a2c2868db705c262d35decefda2a0b3
hanssoto-cyber <hans.soto.g@gmail.com>
2026-04-01 21:55:27 -0300

    Agrega header con navbar y contador de carrito actualizado2

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,13 @@
 // aqui el codigo js o jquery para el proyecto de ecommerce
+<script>
+        let contador = 0;
+
+        function actualizarContador() {
+            document.getElementById("contador-carrito").textContent = contador;
+        }
+
+        function agregarAlCarrito() {
+            contador++;
+            actualizarContador();
+        }
+    </script>
